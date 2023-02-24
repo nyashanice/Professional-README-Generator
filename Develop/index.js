a78bfd2141bb60c46f99b9da394ca46c91c24795
Nya Tyus <nyastyus@gmail.com>
2023-02-23 20:42:14 -0600

    use inquirer to make questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,72 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const readme = async () => {
+  const questions = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your project's title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Please enter your project description.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "What is the usage information?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What are the contribution guidelines?",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "What are the test instructions?",
+      name: "test",
+      choices: [
+        "None",
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+        "BSD 2-Clause Simplified License",
+        "BSD 3-Clause New or Revised License",
+        "Boost Software License 1.0",
+        "Creative Commons Zero v1.0 Universal",
+        "Eclipse Public License 2.0",
+        "GNU Affero Genral Public License v3.0",
+        "GNU General Public License v2.0",
+        "GNU Lesser General Public License v2.1",
+        "Mozilla Public License 2.0",
+        "The Unilicense",
+      ],
+    },
+    {
+      type: "list",
+      message: "Please choose a license",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
